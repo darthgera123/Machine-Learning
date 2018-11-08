82df099c9d8c9beddaf3d0432afb00c78eabcda2
Pulkit gera <pulkithifi@yahoo.com>
2018-11-08 06:08:42 +0530

    recommender added

--- recommender/main.py
+++ recommender/main.py
@@ -4,8 +4,6 @@ import data
 
 # Importing Data
 db = data.data
-print(db)
-
 """ Now in order to recommend a book to someone, we need to find similarity between 2 people
 In order to do that, we have 2 ways - Euclidean method and Pearson method """
 
@@ -40,6 +38,8 @@ similarity = euclidean('Michael Stonebraker', 'Alan Perlis')
 print(similarity)
 """ Doesnt work best because it can be skewed with the ratings and doesnt really form a correlation """
 
+""" Now we use pcc. It isnt exactly a full blown matrix. Here however what we do is use pearson coefficient to understand similarity
+between people. This is a better measure as compared to euclidean measure.   """
 def pcc(p1,p2):
     common_ranked = common_ranked = [itm for itm in db[p1] if itm in db[p2]]
     n = len(common_ranked)
@@ -53,3 +53,36 @@ def pcc(p1,p2):
     return (num/den) if den != 0 else 0
 similarity = pcc('Michael Stonebraker', 'Alan Perlis')
 print(similarity)
+
+
+""" Now we are using any one of the similarity functions created and multiply it with all the weights """
+""" This will give us the highest score and the highest recommendation """
+def recommend(person,bound,similarity):
+    scores = [(similarity(person,other),other) for other in db if other != person ]
+    scores.sort()
+    scores.reverse()
+    scores = scores[0:bound]
+    recomms = {}
+
+    for sim,other in scores:
+        ranked = db[other]
+
+        for itm in ranked:
+            if itm not in db[person]:
+                weight = sim * ranked[itm]
+
+                if itm in recomms:
+                    s, weights = recomms[itm]
+                    recomms[itm] = (s+sim,weights+[weight])
+                else:
+                    recomms[itm] = (sim,[weight])
+        
+    for r in recomms:
+        sim, item = recomms[r]
+        recomms[r] = sum(item) / sim
+    
+    return(recomms)
+
+print(recommend('Alan Perlis',10,pcc))
+
+""" Next up implementing cosine similarity """
